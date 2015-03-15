84985cc52f34474fb8f736e5b373e3deaa3fc02f
prabhu1010 <prangarajan@hcl.com>
2015-03-15 14:06:37 +0530

    Create Utils.h

---
+++ Utils.h
@@ -0,0 +1,13 @@
+#ifndef _COMPARE_N_COPY_UTILS_
+#define _COMPARE_N_COPY_UTILS_
+
+#define CMP_UTILS_DATE    " Date"
+#define CMP_UTILS_NUMBER  " Number"
+#define CMP_UTILS_VARCHAR " String"
+
+#define COMPARE_FILE_PATH "V:\\OracleHaCpp\\SampleData_Pblm1\\SampleData\\Challenge1\\"
+#define COMPARE_DELIVERY_TABLE "NewDeliveryTable.csv"
+#define COMPARE_CUSTOMER_TABLE "NewCustomerTable.csv"
+#define COMPARE_REF_TABLE "Challenge1_InitialData1.csv"
+
+#endif
